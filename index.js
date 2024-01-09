f2f879cc5399bffac373446eea21f6ad8ca26d39
Shay Okamura <115299115+shay-okamura@users.noreply.github.com>
2024-01-08 16:57:23 -1000

    Update index.js
    Signed-off-by: Shay Okamura <115299115+shay-okamura@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
-var dal = require('./dal.js');
+function Spa() {
+  return (
+    <HashRouter>
+      <NavBar/>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+        <div className="container" style={{padding: "20px"}}>
+          <Route path="/" exact component={Home} />
+          <Route path="/CreateAccount/" component={CreateAccount} />
+          <Route path="/deposit/" component={Deposit} />
+          <Route path="/withdraw/" component={Withdraw} />
+          <Route path="/alldata/" component={AllData} />
+        </div>
+      </UserContext.Provider>      
+    </HashRouter>
+  );
+}
 
-app.use(express.static('public'));
-app.use(cors());
-
-app.get('/account/create/:name/:email/:passwork', function (req, res){
-    dal.create(req.params.name,req.params.email,req.params.password).
-    then((user) => {
-        console.log(user);
-        res.send(user);
-    });
-});
-
-
-app.get('/account/all', function (req, res) {
- dal.all().
- then((docs) => {
-    console.log(docs);
-    res.send(docs);
- });
-});
+ReactDOM.render(
+  <Spa/>,
+  document.getElementById('root')
+);
 
 var port = 3000;
 app.listen(port);
