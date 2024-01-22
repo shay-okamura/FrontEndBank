8348c66250a8f65fbef18faa151b70e97f2e3a66
Shay Okamura <115299115+shay-okamura@users.noreply.github.com>
2024-01-22 10:53:28 -1000

    Update home.js
    Signed-off-by: Shay Okamura <115299115+shay-okamura@users.noreply.github.com>

--- home.js
+++ home.js
@@ -2,8 +2,8 @@ function Home(){
   return (
     <Card
       txtcolor="black"
-      header="BadBank Landing Module"
-      title="Welcome to the bank"
+      header="Bank Landing Module"
+      title="Welcome to the Bank"
       text="You can move around using the navigation bar."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
     />
